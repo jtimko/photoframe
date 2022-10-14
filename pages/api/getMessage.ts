a3c0d95b96269e25ec5ab37ac0737d7721cfa457
jtimko <justin@jtimko.com>
2022-10-13 17:12:29 -0700

    first init

---
+++ pages/api/getMessage.ts
@@ -0,0 +1,9 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import twilio from 'twilio';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const accountSid = process.env.TWILIO_ACCOUNT_SID;
+    const token = process.env.TWILIO_AUTH_TOKEN;
+
+    const client = twilio(accountSid, token);
+}
