f5506af04cd285e37aac46999113ff78cf73d624
jtimko <justin@jtimko.com>
2022-10-14 19:56:49 -0700

    maybe

--- pages/api/getMessage.ts
+++ pages/api/getMessage.ts
@@ -1,18 +1,14 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import twilio from 'twilio';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    const accountSid = process.env.TWILIO_ACCOUNT_SID;
-    const token = process.env.TWILIO_AUTH_TOKEN;
-
-    const client = twilio(accountSid, token);
-
     const data = req.body;
-
     const supportedMimes = ["image/jpeg", "image/jpg", "image/png"];
 
     if (data.MediaContentType0 && supportedMimes.includes(data.MediaContentType0)) {
-        console.log("True");
+        const file = await fetch(data.MediaUrl0).then(async d => {
+            const s = await d;
+            console.log(JSON.stringify(d));
+        })
     } else {
         console.log("Nope");
     }
