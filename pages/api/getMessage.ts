117ba0fdb1d64409e326821d5cc91b109438de4b
jtimko <justin@jtimko.com>
2022-10-21 17:51:16 -0700

    added saveas lib

--- pages/api/getMessage.ts
+++ pages/api/getMessage.ts
@@ -1,14 +1,16 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { saveAs } from "file-saver";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const data = req.body;
     const supportedMimes = ["image/jpeg", "image/jpg", "image/png"];
 
     if (data.MediaContentType0 && supportedMimes.includes(data.MediaContentType0)) {
-        const file = await fetch(data.MediaUrl0).then(async d => {
-            const s = await d;
-            console.log(JSON.stringify(d));
-        })
+        // const file = await fetch(data.MediaUrl0).then(async d => {
+        //     const s = await d;
+        //     console.log(JSON.stringify(d));
+        // })
+        saveAs(data.MediaUrl0, "poster.jpg");
     } else {
         console.log("Nope");
     }
