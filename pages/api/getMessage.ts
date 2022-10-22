069b9ebabdd1c5ff2f77f9b7c87df4e8da606975
jtimko <justin@jtimko.com>
2022-10-21 19:11:21 -0700

    no idea

--- pages/api/getMessage.ts
+++ pages/api/getMessage.ts
@@ -1,16 +1,15 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { saveAs } from "file-saver";
+import { readFileSync, writeFileSync } from "fs";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const data = req.body;
     const supportedMimes = ["image/jpeg", "image/jpg", "image/png"];
 
     if (data.MediaContentType0 && supportedMimes.includes(data.MediaContentType0)) {
-        // const file = await fetch(data.MediaUrl0).then(async d => {
-        //     const s = await d;
-        //     console.log(JSON.stringify(d));
-        // })
-        saveAs(data.MediaUrl0, "poster.jpg");
+        //const file = await fetch(data.MediaUrl0);
+        const image = readFileSync(data.MediaUrl0);
+        writeFileSync('../../public/', image);
+
     } else {
         console.log("Nope");
     }
