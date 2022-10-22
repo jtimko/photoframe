d1b41b9d3fa4ea710859ba29b8adf6a6c9e1f310
jtimko <justin@jtimko.com>
2022-10-21 19:28:46 -0700

    getcircle

--- pages/api/getMessage.ts
+++ pages/api/getMessage.ts
@@ -8,8 +8,21 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     if (data.MediaContentType0 && supportedMimes.includes(data.MediaContentType0)) {
         await fetch(data)
         .then(d => d.json())
-        .then(json => console.log(json));
+        .then(json => console.log(json, getCircularReplacer()));
     } else {
         console.log("Nope");
     }
 }
+
+const getCircularReplacer = () => {
+    const seen = new WeakSet();
+    return (key: any, value: any) => {
+      if (typeof value === 'object' && value !== null) {
+        if (seen.has(value)) {
+          return;
+        }
+        seen.add(value);
+      }
+      return value;
+    };
+  };
