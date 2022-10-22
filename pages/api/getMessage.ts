d4150323278a403b012e1f210ad9409774731ad7
jtimko <justin@jtimko.com>
2022-10-21 19:16:29 -0700

    check blob

--- pages/api/getMessage.ts
+++ pages/api/getMessage.ts
@@ -6,9 +6,10 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const supportedMimes = ["image/jpeg", "image/jpg", "image/png"];
 
     if (data.MediaContentType0 && supportedMimes.includes(data.MediaContentType0)) {
-        //const file = await fetch(data.MediaUrl0);
-        const image = readFileSync(data.MediaUrl0);
-        writeFileSync('../../public/', image);
+        const file = await fetch(data.MediaUrl0);
+        console.log(JSON.stringify(file.blob));
+        // const image = readFileSync(file.);
+        // writeFileSync('../../public/', image);
 
     } else {
         console.log("Nope");
